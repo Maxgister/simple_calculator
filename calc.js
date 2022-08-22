623b3b2830faaeabf684da81b4b52bdfde02fad8
Maxim Tsarkov <mu.tsarkov@gmail.com>
2022-08-23 00:35:04 +0200

    11/18 errors passed

--- calc.js
+++ calc.js
@@ -1,48 +1,37 @@
 function calculator(string) {
   // write your code here
-  
   let numbers = string.split(' '),
-      numbersArab = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-      numbersRome = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X'],
+      Arab = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
+      Rome = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X'],
       operators = ['/', '*','+', '-'];
 
   let x = numbers[0],
       y = numbers[2],
-      z = numbers[1];
-  let result = '';
+      z = numbers[1],
+      result = null;
 
-    //ПРОВЕРЯЕМ РИМСКИЕ ЦИФРЫ I <= X
-    function checkElementRome (arg) {
-        for (let i = 0; i < numbersRome.length; i++) {
-            if (numbersRome[i] == arg) {
-                return true;
-            }
-        }
-        return false;
+
+    function checkType (type, arg) {
+      if (numbers.length <= 3) {
+        for (let i = 0; i < type.length; i++) {
+          switch (arg) {
+            case Rome[i]:
+              return 'Rome';
+            case Arab[i]:
+              return 'Arab';
+            case !Rome:
+              return false;
+            case !Arab:
+              return false;
+          }
+        }    
+      } else {
+        throw Error();
+      }
     }
 
-    //ПРОВЕРЯЕМ АРАБСКИЕ ЦИФРЫ  1 <= 10
-    function checkElementArab (arg) {
-    for (let i = 0; i < numbersArab.length; i++) {
-        if (numbersArab[i] === Number(arg) ) {
-                return true;
-            }
-        }
-    return false;
-    }
-
-    //ПРОВЕРЯЕМ ОПЕРАТОРЫ
-    function checkOperator (arg) {
-        for (let i = 0; i < operators.length; i++) {
-            if (operators[i] == (arg) ) {    
-                return true;
-                }
-            }
-        return false;
-        }
-    
     //ИЗ РИМСКИХ В АРАБСКИЕ
-    function deromanization (roman) {
+    function toArab (roman) {
         roman = roman.toUpperCase();
         let lookup = {I:1,V:5,X:10,L:50,C:100,D:500,M:1000},
             arabic = 0,
@@ -57,16 +46,20 @@ function calculator(string) {
       }
     
     //ИЗ АРАБСКИХ В РИМСКИЕ 
-    function romanization (arg) {
+    function toRome (arg) {
         let dict = {M:1000, CM:900, D:500, CD:400, C:100, XC:90, L:50, XL:40, X:10, IX:9, V:5, IV:4, I:1},
             roman = '',
             i;
-        for ( i in dict ) {
+        if (arg > 0) {
+            for ( i in dict ) {
           while ( arg >= dict[i] ) {
             roman += i;
             arg -= dict[i];
           }
         }
+      } else {
+            return '';
+      }
         return roman;
       }
 
@@ -74,7 +67,8 @@ function calculator(string) {
     function calculation () {
         x = Number(x);
         y = Number(y);
-        switch (z) {
+        if (operators.includes(z)) {
+          switch (z) {
             case '+':
                 result = x + y;
                 break;
@@ -88,22 +82,26 @@ function calculator(string) {
                 result = Math.floor(x / y);
                 break;
         }
+      } else {
+        throw Error();
+      }
     }
 
-    switch (checkElementRome(x) && checkElementRome(y) || !checkElementArab(x) && !checkElementArab(y)) {
-        case false:
+    
+   switch (checkType(Arab, x) && checkType(Arab, y) || checkType(Rome, x) && checkType(Rome, y)) {
+        case 'Arab':
             calculation();
             return `${result}`;
-        case true:
-            x = deromanization(x);
-            y = deromanization(y);
+        case 'Rome':
+            x = toArab(numbers[0]);
+            y = toArab(numbers[2]);
             calculation();
-            result = romanization(result);
+            result = toRome (result);
             return `${result}`;
-        default :
-            return Error();
+        default:
+            throw Error();
         }
-
-}
+    
+} 
 
 module.exports = calculator;
